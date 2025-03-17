1c7f7d3c7e93195e6db222c36e78a709c9c2cc00
nyeonk-devcra <nyeonk@devcra.com>
2025-03-18 02:57:39 +0900

    [ fix ] React 18, Next.js 14 및 호환 가능한 Tailwind 버전으로 다운그레이드

--- src/app/csr/page.tsx
+++ src/app/csr/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import Counter from "@/components/Counter";
 import NavigationBar from "@/components/NavigationBar";
 import { Suspense } from "react";
 
@@ -9,11 +10,10 @@ export default async function CSRPage() {
     <div className="flex flex-col h-full">
       <NavigationBar session={session} />
       <main className="isolate flex-1 pt-16">
-        <div className="mx-auto container max-w-7xl px-6 sm:px-6 lg:px-8">
+        <div className="mx-auto container h-full max-w-7xl px-6 sm:px-6 lg:px-8">
+          <Counter />
           <Suspense fallback={<div>Loading...</div>}>
-            <div className="container mx-auto max-w-7xl">
-              <div>Hello, world?</div>
-            </div>
+            <div className="container mx-auto max-w-7xl"></div>
           </Suspense>
         </div>
       </main>
